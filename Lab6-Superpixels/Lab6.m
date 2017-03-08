e145ff79a1b5d62060af3e38b846c392251bb48a
laubravo <lm.bravo10@uniandes.edu.co>
2017-03-07 21:11:38 -0500

    esta también sirve!

--- Lab6-Superpixels/Lab6.m
+++ Lab6-Superpixels/Lab6.m
@@ -1,5 +1,5 @@
 %% Lab 6 - Superpixels
-% clear all; 
+%clear all; 
 close all;clc
 addpath(fullfile('Lab5Images'));
 %% Evaluate methods
@@ -12,31 +12,36 @@ the_best = [1,1,1,4,1,5,4,4];
 % initialize gt
 num_gt = numel(gt_names);
 info = cell(num_gt,2);
-
-%%
-% Evaluate all the methods
+% for ind = 1:num_gt;
+%     actual = load(fullfile('Lab5Images',gt_names(the_best(ind)).name));
+%     info{ind,1} = actual.groundTruth{1}.Segmentation;
+%     actual = imread(im_names(ind).name);
+%     info{ind,2} = actual(:,:,1);
+% end
+%% Initialize vars
 methods = {'k-means', 'gmm', 'hierarchical', 'watershed'};
 color_options = {'rgb', 'lab', 'hsv', 'rgb+xy', 'lab+xy', 'hsv+xy'};
 num_ims = numel(im_names);
-score = zeros(num_ims,numel(color_options),length(methods));
-
-
-for im = 1:num_gt;
-    actual_gt = load(fullfile('Lab5Images',gt_names(the_best(im)).name));
-    actual_gt = actual_gt.groundTruth{1}.Segmentation;
-    actual_im = imread(im_names(im).name);
-    num_clusts = max(max(actual_gt));
-    for method = 1:length(methods)
-        for co = 1: length(color_options)    
-        seg = segmentByClustering(actual_im, color_options{co}, methods{method}, num_clusts);
-        score(im, co, method) = sum(and(seg,actual_gt))/sum(or(seg,actual_gt));
+% gt = cell(num_ims,1);
+gt = cell(2,1);
+%% Fill array of segmentations
+% segs = cell(num_ims,length(methods), length(color_options));
+segs = cell(2,1,1);
+for im = 1:2
+    % for im = 1:num_ims
+    gt_actual = load(fullfile('Lab5Images',gt_names(im).name));
+    gt_actual = gt_actual.groundTruth;
+    gt{im} = gt_actual{the_best(im)}.Segmentation;
+    num_clusts = max(max(gt{im}));
+    %     for method = 1:length(methods)
+    for method = 1:1
+        %         for co = 1: length(color_options)
+        for co = 1:1
+            actual_im = imread(im_names(co).name);
+            segs{im, method, co} = segmentByClustering(actual_im, color_options{co}, methods{method}, num_clusts);
         end
     end
 end
-
-
-
-
-
-
+%% Evaluate Segmentation
+score = evaluateSegmentation(gt,segs);
 
