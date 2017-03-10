36dd71160867f20dd2a5515c30abf3d6f29ca5da
laubravo <lm.bravo10@uniandes.edu.co>
2017-03-09 23:36:09 -0500

    lee toda las ims y no se queda en la primera

--- Lab6-Superpixels/Lab6.m
+++ Lab6-Superpixels/Lab6.m
@@ -11,7 +11,7 @@ gt_names = dir(fullfile('Lab5Images','*.mat'));
 the_best = [1,1,1,4,1,5,4,4];
 % initialize gt
 num_gt = numel(gt_names);
-info = cell(num_gt,2);
+% info = cell(num_gt,2);
 % for ind = 1:num_gt;
 %     actual = load(fullfile('Lab5Images',gt_names(the_best(ind)).name));
 %     info{ind,1} = actual.groundTruth{1}.Segmentation;
@@ -21,27 +21,30 @@ info = cell(num_gt,2);
 %% Initialize vars
 methods = {'k-means', 'gmm', 'hierarchical', 'watershed'};
 color_options = {'rgb', 'lab', 'hsv', 'rgb+xy', 'lab+xy', 'hsv+xy'};
-num_ims = numel(im_names);
-% gt = cell(num_ims,1);
-gt = cell(2,1);
+% num_ims = numel(im_names);
+num_ims = 4;
+gt = cell(num_ims,1);
+% gt = cell(2,1);
 %% Fill array of segmentations
-% segs = cell(num_ims,length(methods), length(color_options));
-segs = cell(2,1,1);
-for im = 1:2
-    % for im = 1:num_ims
+segs = cell(num_ims,length(methods), length(color_options));
+%  segs = cell(2,1,2);
+% for im = 1:2
+for im = 1:4
     gt_actual = load(fullfile('Lab5Images',gt_names(im).name));
     gt_actual = gt_actual.groundTruth;
     gt{im} = gt_actual{the_best(im)}.Segmentation;
     num_clusts = max(max(gt{im}));
-    %     for method = 1:length(methods)
-    for method = 1:1
-        %         for co = 1: length(color_options)
-        for co = 1:1
-            actual_im = imread(im_names(co).name);
+    for method = 1:length(methods)
+        %     for method = 1:1
+        for co = 1: length(color_options)
+            %         for co = 1:2
+            actual_im = imread(im_names(im).name);
             segs{im, method, co} = segmentByClustering(actual_im, color_options{co}, methods{method}, num_clusts);
         end
     end
 end
+
+
+
 %% Evaluate Segmentation
 score = evaluateSegmentation(gt,segs);
-
